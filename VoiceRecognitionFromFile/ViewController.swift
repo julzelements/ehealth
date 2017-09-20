8ef5d7184871a5451645885b7f1662c29225a222
Julian Scharf <jscharf@thoughtworks.com>
2017-09-20 17:58:46 +1000

    Initial Commit

---
+++ VoiceRecognitionFromFile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceRecognitionFromFile
+//
+//  Created by Julian Scharf on 20/9/17.
+//  Copyright © 2017 Julian Scharf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

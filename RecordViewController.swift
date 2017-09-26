5053cfc73b35095b9a829e0cfbef47bcbf1ce675
Julian Scharf <jscharf@thoughtworks.com>
2017-09-26 13:34:13 +1000

    about to remove the hold to record funtionality

--- RecordViewController.swift
+++ RecordViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class RecordViewController: UIViewController {
     
+    var apiMockTimer: Timer!
+    
     @IBOutlet weak var microphoneView: UIView!
     @IBOutlet weak var instructionsLabel: UILabel!
     @IBOutlet weak var eqVisualiserView: UIImageView!
@@ -18,6 +20,7 @@ class RecordViewController: UIViewController {
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var transcribeButton: UIButton!
     @IBOutlet weak var recordLabel: UILabel!
+    
     
 
     enum State {
@@ -28,6 +31,7 @@ class RecordViewController: UIViewController {
         case paused
         case recognitionCompleted
         case playingRecording
+        case audioHasBeenTranscribed
     }
     
     override func viewDidLoad() {
@@ -44,23 +48,23 @@ class RecordViewController: UIViewController {
         switch state {
         case .inactiveNoPayload:
             recordLabel.text = "inactiveNoPayload"
-            playButton.isHidden = true
-            transcribeButton.isHidden = true
+            playButton.isEnabled = false
+            transcribeButton.isEnabled = false
             recordButton.setTitle("Record", for: .normal)
             resetClock(label: clockLabel)
         case .recording:
             recordLabel.text = "recording"
-            playButton.isHidden = true
-            recordButton.isHidden = false
-            transcribeButton.isHidden = true
+            playButton.isEnabled = false
+            recordButton.isEnabled = true
+            transcribeButton.isEnabled = false
             startClock(label: clockLabel)
         case .transcribingAudio:
             recordLabel.text = "transcribingAudio"
-            playButton.isHidden = true
+            playButton.isEnabled = false
             instructionsLabel.text = "audio is being transcribed"
-            transcribeButton.isHidden = true
-            recordLabel.isHidden = true
-            recordButton.isHidden = true
+            transcribeButton.isEnabled = false
+            recordLabel.isEnabled = false
+            recordButton.isEnabled = false
         case .playingRecording:
             recordLabel.text = "playingRecording"
             instructionsLabel.text = "playing recording"
@@ -68,8 +72,13 @@ class RecordViewController: UIViewController {
         case .inactiveWithPayload:
             recordLabel.text = "inactiveWithPayload"
             instructionsLabel.text = "Press rec to continue recording\nPress Transcribe to transcribe audio"
-            transcribeButton.isHidden = false
+            transcribeButton.isEnabled = true
             pauseClock(label: clockLabel)
+        case .audioHasBeenTranscribed:
+            recordLabel.text = "inactiveWithPayload"
+            instructionsLabel.text = "Audio has been transcribed"
+            playButton.isEnabled = true
+            
         default:
             return
         }
@@ -78,6 +87,7 @@ class RecordViewController: UIViewController {
     @IBAction func transcribeRecording(_ sender: Any) {
         updateUI(state: .transcribingAudio)
         pauseClock(label: clockLabel)
+        callApi()
     }
     
     @IBAction func playRecording(_ sender: Any) {
@@ -103,6 +113,15 @@ class RecordViewController: UIViewController {
     
     func resetClock(label: UILabel) {
         label.text = "00:00:00"
+    }
+    
+    func  callApi() {
+        apiMockTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(transriptionComplete), userInfo: nil, repeats: false)
+    }
+    
+    @objc func transriptionComplete() {
+        updateUI(state: .audioHasBeenTranscribed)
+        
     }
     
 }
